5f7f4d4ff9e60dfa92b39245a005bbd20f72ac84
Md Rabby <ablzerabbi159@gmail.com>
2023-08-14 21:05:35 +0600

    added addEventLisetener in login button

--- connecting.js
+++ connecting.js
@@ -1,1 +1,20 @@
-console.log('hello world');
+// click event handler with login btn
+document.getElementById('btn-submit').addEventListener('click', function(){
+    //get the email address inside the email input field
+    const emailField = document.getElementById('user-email');
+    const getEmail = emailField.value;
+    
+
+    // get password & set id on the html element & get the element & get the value from the element
+    const passwordField = document.getElementById('user-password');
+    const getPassword = passwordField.value;
+    
+    
+    //verify email and password (do not use farther)
+    if(getEmail === 'selfbank24@gmail.com' && getPassword === 'mybank'){
+        console.log('valid user');
+    }
+    else{
+        console.log('invalid user');
+    }
+});
