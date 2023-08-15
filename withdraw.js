ed9b134d132bd240df1627284917905a8ac971ab
Md Rabby <ablzerabbi159@gmail.com>
2023-08-15 18:56:15 +0600

    update 0.1

--- withdraw.js
+++ withdraw.js
@@ -4,24 +4,37 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     const withdrawAmount = document.getElementById('withdraw-amount');
     const getNewWithdrawString = withdrawAmount.value;
     const getNewWithdraw = parseFloat(getNewWithdrawString);
+    if(isNaN(getNewWithdraw)){
+        alert('Please provide a valid amount');
+        return withdrawAmount.value = '';
+    }
 
      //get the withdraw total amount
      const withdrawTotalAmount = document.getElementById('withdraw-total');
      const previousWithdrawTotalString = withdrawTotalAmount.innerText;
      const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
 
-     //total withdraw calculate
-    const currentWithdrawTotal = previousWithdrawTotal + getNewWithdraw;
-    withdrawTotalAmount.innerText = currentWithdrawTotal;
+     
 
     //get the balance total amount
     const balanceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    if(getNewWithdraw > previousBalanceTotal){
+        alert('Influence balance')
+        return withdrawAmount.value = '';
+    }
     
+    //total withdraw calculate
+    const currentWithdrawTotal = previousWithdrawTotal + getNewWithdraw;
+    withdrawTotalAmount.innerText = currentWithdrawTotal;
+
+
     //total balance calculate
     const currentBalanceTotal = previousBalanceTotal - getNewWithdraw;
     balanceTotalElement.innerText = currentBalanceTotal;
+    
 
      // deposit input field clear 
      withdrawAmount.value = '';
